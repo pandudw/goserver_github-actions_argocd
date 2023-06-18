cd675d1a0c059b5dcf639cc4335a2b5583b4f71c
Nasrullah Pandu Dewantara <nasrullah.pandud1@gmail.com>
2023-06-18 16:17:23 +0700

    add initial argocd

--- main.go
+++ main.go
@@ -1,27 +1,29 @@
 package main
 
-import "fmt"
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
-    var message = "Selamat Datang di Dunia DevOps!"
-    w.Write([]byte(message))
+	var message = "Selamat Datang di Dunia DevOps!"
+	w.Write([]byte(message))
 }
 
 func handlerHello(w http.ResponseWriter, r *http.Request) {
-    var message = "Hello, Mari Kita Belajar Bersama"
-    w.Write([]byte(message))
+	var message = "Hello, Mari Kita Belajar Bersama"
+	w.Write([]byte(message))
 }
 
 func main() {
-    http.HandleFunc("/", handlerIndex)
-    http.HandleFunc("/index", handlerIndex)
-    http.HandleFunc("/hello", handlerHello)
+	http.HandleFunc("/", handlerIndex)
+	http.HandleFunc("/index", handlerIndex)
+	http.HandleFunc("/hello", handlerHello)
 
 	var address = "localhost:9000"
-    fmt.Printf("server started at %s\n", address)
-    err := http.ListenAndServe(address, nil)
-    if err != nil {
-        fmt.Println(err.Error())
-    }
+	fmt.Printf("server started at %s\n", address)
+	err := http.ListenAndServe(address, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
